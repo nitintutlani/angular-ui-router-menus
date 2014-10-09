96f73f928374b581338b1418ca2e43bc1b43a6f2
Nitin Tutlani <nitintutlani@yahoo.com>
2014-10-09 17:39:50 +0530

    Added testMenuStates and testMenuElement for testing tag filter support in menus service and directive

--- test/helpers.js
+++ test/helpers.js
@@ -21,3 +21,64 @@ var stringMenuElement = '<ol menus>' +
     '<li ng-repeat="menu in menus">' +
     '<a ui-sref="{{menu.state.name}}">{{menu.name}}</a>' +
     '</li></ol>';
+
+var tagMenuStates = {
+  'company': {
+    url: '/company',
+    menu: {
+      name: 'Company',
+      tag: 'company'
+    }
+  },
+  'company.about': {
+    url: '/about',
+    menu: {
+      name: 'About Us',
+      tag: 'company'
+    }
+  },
+  'other': {
+    url: '/other',
+    menu: {
+      name: 'Other',
+      tag: 'other'
+    }
+  }
+};
+
+var tagMenuElement = '<ol menus menus-tag="company">' +
+    '<li ng-repeat="menu in menus">' +
+    '<a ui-sref="{{menu.state.name}}">{{menu.name}}</a>' +
+    '</li></ol>';
+
+
+/* Complex
+ states = {
+ 'home': {
+ url: '/home',
+ templateUrl: 'view/index.html',
+ menu: 'Dashboard'
+ },
+ 'users': {
+ url: '/users',
+ templateUrl: 'view/users.list.html',
+ controller: 'UsersListController',
+ menu: {
+ name: 'Users management',
+ class: {
+ active: 'active',
+ hasChild: 'parent',
+ custom: 'bg-green fg-yellow'
+ }
+ }
+ },
+ 'users.edit': {
+ url: '/edit/{id}',
+ templateUrl: 'view/users.edit.html',
+ controller: 'UsersEditController',
+ menu: {
+ exclude: true //excluded menu item
+ }
+ }
+ };
+ */
